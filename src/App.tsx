2b347eff0b95d48aff0e239e963091570bcc6d69
Frank Adler <hi@adfr.io>
2020-03-02 17:04:27 +0100

    :bug: fixed toggle button icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, { ReactElement, useCallback, useState, useEffect } from 'react';
-import {Provider, useDispatch} from 'react-redux';
-import store from './store/Store';
-import Form from './components/Form';
-import styled from 'styled-components';
-import { ipcRenderer } from 'electron';
+import React, { ReactElement, useCallback, useState, useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import store from "./store/Store";
+import Form from "./components/Form";
+import styled from "styled-components";
+import { ipcRenderer } from "electron";
 import DetailView from "./components/DetailView";
 
 const App = (): ReactElement => {
   const [isCollapsed, setCollapsed] = useState<boolean>(false);
 
   useEffect(() => {
-    ipcRenderer.send(`window-${isCollapsed ? 'collapse' : 'expand'}`);
+    ipcRenderer.send(`window-${isCollapsed ? "collapse" : "expand"}`);
   }, [isCollapsed]);
 
   useEffect(() => {
-    ipcRenderer.on('window-collapse', () => {
+    ipcRenderer.on("window-collapse", () => {
       setCollapsed(true);
     });
 
-    ipcRenderer.on('window-expand', () => {
+    ipcRenderer.on("window-expand", () => {
       setCollapsed(false);
     });
   }, []);
@@ -49,7 +49,7 @@ const StyledToggleButton = styled.button`
   flex: 0 0 auto;
   appearance: none;
   background-color: transparent;
-  background-image: url("../iconTemplate@2x.png");
+  background-image: url("../assets/icon@2x.png");
   background-repeat: no-repeat;
   background-position: center;
   background-size: 20px 20px;
