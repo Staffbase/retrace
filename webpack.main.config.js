e5a80a94c12d3d660fc9dab5a63d26bf91843461
Marvin Zeising <marvin.zeising@gmail.com>
2021-12-01 11:08:33 +0100

    fix tray icon for light and dark menubars

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -49,20 +49,12 @@ module.exports = {
   plugins: [
     new CopyPlugin([
       {
-        from: "src/main/assets/icon_dark.png",
-        to: "assets/icon_dark.png",
+        from: "src/main/assets/iconTemplate.png",
+        to: "assets/iconTemplate.png",
       },
       {
-        from: "src/main/assets/icon_dark@2x.png",
-        to: "assets/icon_dark@2x.png",
-      },
-      {
-        from: "src/main/assets/icon_light.png",
-        to: "assets/icon_light.png",
-      },
-      {
-        from: "src/main/assets/icon_light@2x.png",
-        to: "assets/icon_light@2x.png",
+        from: "src/main/assets/iconTemplate@2x.png",
+        to: "assets/iconTemplate@2x.png",
       },
       {
         from: "src/main/config.default.json",
