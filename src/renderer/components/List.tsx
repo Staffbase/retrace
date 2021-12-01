db103080d0cd090d3da052b7312fc9732fb571f6
Marvin Zeising <marvin.zeising@gmail.com>
2021-12-01 14:39:08 +0100

    extract ListItem component with edit functionality

--- src/renderer/components/List.tsx
+++ src/renderer/components/List.tsx
@@ -16,13 +16,9 @@ limitations under the License.
 
 import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { StoreState, Item } from "../store/Types";
 import styled from "styled-components";
-import { HASHTAG_REGEX, MENTION_REGEX } from "../utils";
-
-const lpad = (n: number): string => {
-  return `${n < 10 ? "0" : ""}${n}`;
-};
+import { StoreState, Item } from "../store/Types";
+import ListItem from "./ListItem";
 
 const List = (): ReactElement => {
   const data = useSelector((state: StoreState) => state.data);
@@ -39,30 +35,7 @@ const List = (): ReactElement => {
         ? -1
         : 1;
     })
-    .map((item: Item) => {
-      const created = new Date(item.createdAt);
-
-      return (
-        <StyledListItem className="item" key={item.id} data-item-id={item.id}>
-          <small>
-            {lpad(created.getHours()) + ":" + lpad(created.getMinutes())}
-          </small>
-          <label
-            dangerouslySetInnerHTML={{
-              __html: item.label
-                .replace(
-                  HASHTAG_REGEX,
-                  (str: string) => `<em class="hashtag">${str}</em>`
-                )
-                .replace(
-                  MENTION_REGEX,
-                  (str: string) => `<em class="mention">${str}</em>`
-                ),
-            }}
-          />
-        </StyledListItem>
-      );
-    });
+    .map((item) => <ListItem key={item.id} item={item} />);
 
   return <StyledList>{items}</StyledList>;
 };
@@ -76,36 +49,4 @@ const StyledList = styled.ul`
   padding: 0;
   height: 232px;
   overflow: auto;
-`;
-
-const StyledListItem = styled.li`
-  padding: 8px 15px;
-  color: var(--text);
-  border-bottom: 1px solid var(--border);
-  display: flex;
-  flex-direction: row;
-
-  & > small {
-    color: var(--textDark);
-    margin-right: 15px;
-    flex: 0 0 auto;
-    line-height: 26px;
-  }
-
-  & > label {
-    flex: 1 1 auto;
-    line-height: 26px;
-  }
-
-  & > label > em {
-    font-style: normal;
-  }
-
-  & > label > em.hashtag {
-    color: var(--accentBlue);
-  }
-
-  & > label > em.mention {
-    color: var(--accentRed);
-  }
 `;
