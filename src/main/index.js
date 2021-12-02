f3b08b081c4db19627c3fe7b4dd128d783a6edff
Marvin Zeising <marvin.zeising@gmail.com>
2021-12-02 15:35:56 +0100

    :refactor: refactor menubar registration to typescript class

--- src/main/index.js
+++ src/main/index.js
@@ -14,160 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { menubar } from "menubar";
-import { globalShortcut, ipcMain, Menu } from "electron";
-import { join } from "path";
-import electron, { nativeImage, BrowserWindow } from "electron";
-import config from "../renderer/store/ConfigStore";
+import { ipcMain } from "electron";
+import MenuBar from "./MenuBar";
 
-const getIcon = () => {
-  const path = join(__dirname, `assets/iconTemplate.png`);
-  return nativeImage.createFromPath(path);
-};
-
-const webPreferences = {
-  nodeIntegration: true,
-  nativeWindowOpen: true,
-  devTools: process.env.NODE_ENV !== "production",
-};
-
-const mb = menubar({
-  index: MAIN_WINDOW_WEBPACK_ENTRY,
-  browserWindow: {
-    alwaysOnTop: true,
-    useContentSize: true,
-    transparent: false,
-    width: 500,
-    height: 47,
-    webPreferences,
-  },
-  icon: getIcon(),
-  preloadWindow: true,
-  windowPosition: "trayRight",
-  tooltip: "RE-Trace",
-});
-
-const childWindows = {};
-
-function openChildWindow(path, options = {}) {
-  let win = childWindows[path];
-
-  if (win) {
-    if (win.isMinimized()) {
-      win.restore();
-    }
-
-    win.focus();
-  } else {
-    win = new BrowserWindow({
-      width: 500,
-      height: 500,
-      center: true,
-      resizable: true,
-      show: false,
-      webPreferences,
-      parent: mb.window,
-      ...options,
-    });
-
-    win.loadURL(`${MAIN_WINDOW_WEBPACK_ENTRY}#${path}`).then(() => {
-      win.show();
-    });
-
-    win.on("closed", () => {
-      delete childWindows[path];
-      mb.window.hide();
-    });
-
-    childWindows[path] = win;
-  }
-}
-
-electron.nativeTheme.on("updated", () => {
-  mb.tray.setImage(getIcon());
-});
-
-const collapse = (skipEvent) => {
-  if (!skipEvent) {
-    mb.window.webContents.send("window-collapse");
-  }
-
-  mb.window.setSize(500, 47);
-};
-
-const expand = (skipEvent) => {
-  if (!skipEvent) {
-    mb.window.webContents.send("window-expand");
-  }
-
-  mb.window.setSize(500, 350);
-};
-
-const secondaryMenu = Menu.buildFromTemplate([
-  {
-    label: "History",
-    click: openChildWindow.bind(null, "/history"),
-    accelerator: "CommandOrControl+H",
-  },
-  {
-    label: "Settings",
-    click: openChildWindow.bind(null, "/settings", {
-      width: 400,
-      height: 250,
-      resizable: false,
-    }),
-    accelerator: "CommandOrControl+D",
-  },
-  {
-    label: "Quit",
-    click: mb.app.quit,
-    accelerator: "CommandOrControl+Q",
-  },
-]);
-
-if (mb.app.isPackaged) {
-  mb.app.setLoginItemSettings({
-    openAtLogin: config.get("autostart") === true,
-    openAsHidden: true,
-  });
-}
-
-mb.on("ready", () => {
-  if (!config.get("floatShortcut")) {
-    config.set("floatShortcut", "CommandOrControl+L");
-  }
-
-  globalShortcut.register(config.get("floatShortcut"), () => {
-    // initiate smaller hotkey mode, collapsed and centered
-    collapse();
-
-    mb.setOption("windowPosition", "center");
-    mb.showWindow();
-  });
-
-  mb.tray.on("right-click", () => {
-    mb.tray.popUpContextMenu(secondaryMenu);
-  });
-
-  mb.window.webContents.send("reset-calendar");
-});
-
-mb.on("focus-lost", mb.hideWindow);
-
-mb.on("after-hide", () => {
-  // restore initial menu bar app position
-  mb.setOption("windowPosition", "trayRight");
-  expand();
-
-  mb.window.webContents.send("reset-calendar");
-
-  // restore focus to where it was before
-  mb.app.hide();
-});
+const menuBar = new MenuBar(MAIN_WINDOW_WEBPACK_ENTRY);
 
 ipcMain.on("close-window", () => {
-  mb.hideWindow();
+  menuBar.hideWindow();
 });
 
-ipcMain.on("window-collapse", collapse.bind(null, true));
-ipcMain.on("window-expand", expand.bind(null, true));
+ipcMain.on("window-collapse", menuBar.collapse.bind(null, true));
+ipcMain.on("window-expand", menuBar.expand.bind(null, true));
+ipcMain.on("autostart-changed", menuBar.registerAutoStart);
+ipcMain.on("floatShortcut-changed", menuBar.registerFloatShortcut);
